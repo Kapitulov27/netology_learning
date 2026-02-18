fe995e29729edd318f8f20dc66632561d039ff11
Albert K <kapitulov27@gmail.com>
2026-02-18 20:15:33 +0300

    new task

--- ter-homeworks/01/lecture/main.tf
+++ ter-homeworks/01/lecture/main.tf
@@ -1,25 +1,35 @@
 terraform {
-  required_providers {}
-  required_version = "~>1.12.0"
+  required_providers {
+    docker = {
+      source  = "kreuzwerker/docker"
+    }
+  }
+  required_version = "~>1.12.0" /*Многострочный комментарий.
+ Требуемая версия terraform */
+}
+provider "docker" {}
+
+#однострочный комментарий
+
+resource "random_password" "random_string" {
+  length      = 16
+  special     = false
+  min_upper   = 1
+  min_lower   = 1
+  min_numeric = 1
 }
 
-resource "random_password" "any_uniq_name" {
-  length = 16
+resource "docker_image" {
+  name         = "nginx:latest"
+  keep_locally = true
 }
 
-resource "local_file" "from_resourse" {
-  content  = random_password.any_uniq_name.result
-  filename = "/tmp/from_resource.txt"
-}
+resource "docker_container" "1nginx" {
+  image = docker_image.nginx.image_id
+  name  = "example_${random_password.random_string_FAKE.resulT}"
 
-
-
-
-data "local_file" "version" {
-  filename = "/proc/version"
-}
-
-resource "local_file" "from_dataresourse" {
-  content  = data.local_file.version.content
-  filename = "/tmp/from_data_source.txt"
+  ports {
+    internal = 80
+    external = 9090
+  }
 }
