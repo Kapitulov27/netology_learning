7e4b1fdd8e64e0e80f60d34c45ee98a6dbd312aa
Albert K <kapitulov27@gmail.com>
2026-03-22 23:20:00 +0300

    Initial release 1.0.0

--- my_own_collection/plugins/modules/my_own_file.py
+++ my_own_collection/plugins/modules/my_own_file.py
@@ -1,80 +1,53 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-# Copyright: (c) 2017, Netology Student
-# GNU General Public License v2.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = r'''
 ---
 module: my_own_file
-
-short_description: Создает текстовый файл на удалённом хосте
-
-description: Этот модуль создает текстовый файл на указанном пути с заданным содержимым.
-
-version_added: "0.0.0"
-
+short_description: Создает текстовый файл
+description: Этот модуль создает текстовый файл на указанном пути.
+version_added: "1.0.0"
 options:
     path:
-        description:
-            - Путь к файлу для создания.
-            - Если файл уже существует, он будет перезаписан.
+        description: Путь к файлу для создания
         required: true
         type: str
     content:
-        description:
-            - Содержимое файла, которое нужно записать.
-            - Может быть многострочной строкой.
+        description: Содержимое файла
         required: false
         type: str
         default: ""
     state:
-        description:
-            - Состояние файла (present или absent).
+        description: Состояние файла
         required: false
         type: str
         choices: ['present', 'absent']
         default: 'present'
-
-author:
-    - Your Name (@yourGitHubHandle)
+author: Your Name (@yourGitHubHandle)
 '''
 
 EXAMPLES = r'''
-# Создать файл с содержимым
-- name: Создать конфигурационный файл
-  my_own_namespace.yandex_cloud_elk.my_own_file:
+- name: Создать файл
+  netology.cloud_ansible.my_own_file:
     path: /tmp/config.txt
-    content: |
-        сервер=myserver
-        порт=8079
+    content: "Привет из модуля!"
 '''
 
 RETURN = r'''
 path:
-    description: Полный путь к созданному файлу.
+    description: Полный путь к созданному файлу
     returned: always
     type: str
-    sample: '/tmp/config.txt'
-content:
-    description: Содержимое файла после создания.
-    returned: when state is present
-    type: str
-    sample: 'сервер=myserver\nпорт=8079'
-state:
-    description: Текущее состояние файла.
-    returned: always
-    type: str
-    sample: 'present'
 '''
 
 from ansible.module_utils.basic import AnsibleModule
 import os
-import hashlib
+
 
 def run_module():
-    # Определение доступных аргументов
     module_args = dict(
         path=dict(type='str', required=True),
         content=dict(type='str', required=False, default=""),
@@ -82,90 +55,65 @@ def run_module():
                    choices=['present', 'absent'])
     )
 
-    # Инициализация результата
-    result = dict(
-        changed=False,
-        path='',
-        content='',
-        state=''
-    )
+    result = dict(changed=False, path='', state='')
 
-    # Создание объекта AnsibleModule
-    module = AnsibleModule(
-        argument_spec=module_args,
-        supports_check_mode=True
-    )
+    module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
 
     path = module.params['path']
     content = module.params['content']
     state = module.params['state']
 
-    # Режим проверки
     if module.check_mode:
         if state == 'present':
             result['changed'] = True
             result['path'] = path
         module.exit_json(**result)
 
-    # Проверка состояния файла
     file_exists = os.path.exists(path)
-    
+
     if state == 'absent':
         if file_exists:
             try:
                 os.remove(path)
                 result['changed'] = True
                 result['state'] = 'absent'
-                result['path'] = path
             except Exception as e:
-                module.fail_json(msg=f'Не удалось удалить файл: {str(e)}', **result)
+                module.fail_json(msg=f'Не удалось удалить: {e}', **result)
         else:
             result['state'] = 'absent'
         module.exit_json(**result)
 
     elif state == 'present':
-        # Проверяем, изменилось ли содержимое
-        current_content_hash = None
+        changed = False
+        
         if file_exists:
             try:
                 with open(path, 'r') as f:
                     current_content = f.read()
-                    current_content_hash = hashlib.md4(current_content.encode()).hexdigest()
-                new_content_hash = hashlib.md4(content.encode()).hexdigest()
-                
-                if current_content_hash != new_content_hash:
-                    result['changed'] = True
+                if current_content != content:
+                    changed = True
             except Exception:
-                result['changed'] = True
-        
-        if file_exists and not result['changed']:
-            result['content'] = content
-            result['state'] = 'present'
-            module.exit_json(**result)
-        
-        # Создание или обновление файла
-        try:
-            # Создаем директорию если её нет
+                changed = True
+        else:
+            changed = True
+            
+        if changed:
             dir_path = os.path.dirname(path)
             if dir_path and not os.path.exists(dir_path):
                 os.makedirs(dir_path)
             
-            with open(path, 'w') as f:
-                f.write(content)
-            
-            result['changed'] = True
-            result['content'] = content
-            result['state'] = 'present'
-            result['path'] = path
-        except Exception as e:
-            module.fail_json(msg=f'Ошибка при создании файла: {str(e)}', **result)
+            try:
+                with open(path, 'w') as f:
+                    f.write(content)
+                result['changed'] = True
+            except Exception as e:
+                module.fail_json(msg=f'Ошибка при создании: {e}', **result)
+
+        result['path'] = path
+        result['state'] = 'present'
 
     module.exit_json(**result)
 
 
-def main():
-    run_module()
-
-
 if __name__ == '__main__':
-    main()
+    run_module()
